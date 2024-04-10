cc862722b179629f8baa4c7b9cca17024c833930
luys <sanchezluys@gmail.com>
2024-04-10 17:02:51 -0500

    version beta, cumple con el menu, al terminar cada opcion queda esperando presionar cuaqluier tecla. menu terminado. queda en pruebas

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Iniciando el reto de App Bancaria");
-
-
         //** DEFINIR LAS VARIABLES
         //************************
         String nombreBanco = "Banco-loco";
         String nombreCliente = "Luis Sanchez";
         String moneda="COP";
         String tipoCuenta="Corriente";
+        int saldo=1500;
+        int retiro=0;
+        int deposito=0;
+        String datosCliente= """
+                ***************************
+                Banco: %s
+                Cliente: %s
+                Tipo de Cuenta: %s
+                Saldo Disponible: %s %d
+                ***************************
+                """.formatted(nombreBanco, nombreCliente, tipoCuenta, moneda, saldo);
         //***
         Boolean ciclo=true;
         //***
         int opcion = 1;
-        int saldo=1500;
         Scanner teclado = new Scanner(System.in);
-
         //****************
         while (ciclo) {
+            System.out.println(datosCliente);
             System.out.println("** *********************** ***");
-            System.out.println("** MENU CAJERO AUTOMATICCO ***");
+            System.out.println("** MENU CAJERO AUTOMATICO ***");
             System.out.println("** " + nombreBanco + "     ***");
             System.out.println("** *********************** ***");
             System.out.println("1 Consultar mi saldo");
@@ -35,26 +43,57 @@ public class Main {
             switch (opcion) {
                 case 1:
                     // Código para consultar saldo
-                    System.out.println("Seleccionó Consultar Saldo");
+                    System.out.println("******************************************************");
+                    System.out.println("Sr(a): "+nombreCliente +", saldo actual "+moneda+" "+saldo);
+                    System.out.println("Presione cualquier tecla para continuar");
+                    System.out.println("******************************************************");
                     System.in.read();
                     break;
                 case 2:
                     // Código para realizar un saque
-                    System.out.println("Seleccionó Retirar");
+                    System.out.println("******************************************************");
+                    System.out.println("Sr(a):"+nombreCliente+ "\nSu saldo actual es: "+ moneda + " "+saldo+"\n¿Cuanto desea retirar?");
+                    retiro= teclado.nextInt();
+                    if(retiro > saldo){
+                        System.out.println("******************************************************");
+                        System.out.println("Estimado Sr(a):"+nombreCliente+ "\nSu saldo es insuficiente, recuerde que cuenta con "+moneda+" "+saldo);
+                        System.out.println("Presione cualquier tecla para continuar");
+                        System.out.println("******************************************************");
+                    }
+                    else {
+                        saldo -= retiro;
+                        System.out.println("******* RETIRO EXITOSO *****************************************");
+                        System.out.println("Estimado Sr(a):" + nombreCliente + "\nSu nuevo saldo es " + moneda + " " + saldo);
+                        System.out.println("Presione cualquier tecla para continuar");
+                        System.out.println("******************************************************");
+                    }
                     System.in.read();
                     break;
                 case 3:
                     // Código para realizar un depósito
-                    System.out.println("Seleccionó Depositar");
+                    System.out.println("******************************************************");
+                    System.out.println("Sr(a):"+nombreCliente+ "\nSu saldo actual es: "+ moneda + " "+saldo+"\n¿Cuanto desea depositar?");
+                    deposito= teclado.nextInt();
+                    saldo +=deposito;
+                    System.out.println("******* DEPOSITO EXITOSO *****************************************");
+                    System.out.println("Estimado Sr(a):" + nombreCliente + "\nSu nuevo saldo es " + moneda + " " + saldo);
+                    System.out.println("Presione cualquier tecla para continuar");
+                    System.out.println("******************************************************");
                     System.in.read();
                     break;
                 case 4:
                     // Código para salir del programa
-                    System.out.println("Seleccionó Salir");
+                    System.out.println("******* SALIENDO DEL SISTEMA *****************************************");
+                    System.out.println("Estimado Sr(a):" + nombreCliente + ", " + nombreBanco + " le da las gracias por usar nuestros servicios");
+                    System.out.println("******************************************************");
                     ciclo=false;
                     break;
                 default:
                     // Código para manejar una opción no válida
+                    System.out.println("******************************************************");
+                    System.out.println("Error!! Opcion no valida!! \nPresione cualquier tecla para continuar");
+                    System.out.println("******************************************************");
+                    System.in.read();
                     break;
             }
         }
